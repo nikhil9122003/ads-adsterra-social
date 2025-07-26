1de26451aa9315e954c88db52a218b0d7b970a90
nikhil9122003 <166285467+nikhil9122003@users.noreply.github.com>
2025-07-27 00:16:11 +0530

    Update and rename index.html to ads.js

---
+++ ads.js
@@ -0,0 +1,7 @@
+(function() {
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = 'https://pl27266459.profitableratecpm.com/1f/ae/b3/1faeb304331c7d3cd6d4ea1e6f6ffedf.js';
+  document.body.appendChild(s);
+})();
